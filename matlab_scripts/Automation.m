8adc31179f43e5caa40a84a4712df58bb06899ca
Nishant <nishantnischaya@gmail.com>
2018-10-28 05:08:09 +0530

    update

--- matlab_scripts/Automation.m
+++ matlab_scripts/Automation.m
@@ -1,5 +1,5 @@
 clc;clear;close;
 run c:\Users\nisha\Desktop\matlab_scripts\Sensing.m
-run c:\Users\nisha\Desktop\matlab_scripts\safe_coordinates_generator.m
+run c:\Users\nisha\Desktop\matlab_scripts\alert_points_generator.m
 run c:\Users\nisha\Desktop\matlab_scripts\find_district.m
 run c:\Users\nisha\Desktop\matlab_scripts\posting.m
